4cca317c582b415a9fd4b10826211365849963e4
nastromo <nastromo@Sulako.local>
2019-03-10 12:16:46 +0200

    date picker done

--- src/components/MyDatePicker.js
+++ src/components/MyDatePicker.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+import { SingleDatePicker } from 'react-dates';
+import '../datepicker.css';
+
+
 
 export class MyDatePicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: null,
+            focused: null,
+        };
+    }
 
 
     render() {
         return (
-            <div className="date-picker">1</div>
+            <div className="date-picker">
+                <SingleDatePicker
+                    id="date_input"
+                    numberOfMonths={1}
+                    date={this.state.date}
+                    focused={this.state.focused}
+                    onDateChange={(date) => { this.setState({ date }); }}
+                    onFocusChange={({ focused }) => { this.setState({ focused }); }}
+                    isOutsideRange={() => false}
+                    hideKeyboardShortcutsPanel={true}
+                    daySize={32}
+                />
+            </div>
         )
     }
 }
