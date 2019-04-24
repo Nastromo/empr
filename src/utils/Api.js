8b09ecb258ad0d11fcd48083d70771799c3546c3
nastromo <nastromo@Sulako.local>
2019-04-24 18:02:07 +0300

    url api

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,11 +1,6 @@
 import axios from 'axios';
 
-let url = `http://api.empirecity.ga`;
-if (window.location.href.includes(`localhost`)) {
-    url = `http://localhost:4000`;
-} else if (window.location.href.includes(`192.168`)) {
-    url = `http://192.168.0.105:4000`;
-}
+let url = `http://192.168.20.211:9000`;
 
 
 export default axios.create({
